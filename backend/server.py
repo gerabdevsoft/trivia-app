1561aa7fcb4533661a8f87ad8f1bffeb7ae1483e
gerabdevsoft <gerabdevsoft@gmail.com>
2026-09-15 17:41:00 -0400

    Correcciones produccion backend route y middleware

--- backend/server.py
+++ backend/server.py
@@ -1532,9 +1532,6 @@ async def update_profile(body: UpdateProfileBody, user=Depends(get_current_user)
     fresh = await db.users.find_one({"user_id": user["user_id"]}, {"_id": 0})
     return user_public(fresh)
 
-
-app.include_router(api_router)
-
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
@@ -1542,3 +1539,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+api_router = APIRouter(prefix="/api")
+app.include_router(api_router)
